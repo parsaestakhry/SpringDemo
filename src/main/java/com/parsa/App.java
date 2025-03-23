171832c6a582c36668ced1666c73a7cd074d068e
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-23 22:53:25 +0330

    comment out Alien bean retrieval and coding method call in main

--- src/main/java/com/parsa/App.java
+++ src/main/java/com/parsa/App.java
@@ -15,8 +15,8 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien alien = (Alien)  context.getBean("alien");
-        alien.coding();
+//        Alien alien = (Alien)  context.getBean("alien");
+//        alien.coding();
 
     }
 }
