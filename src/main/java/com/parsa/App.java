ae028b318802ce711cdba685aba02da5d0ea4653
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-23 22:30:40 +0330

    Initial commit

---
+++ src/main/java/com/parsa/App.java
@@ -0,0 +1,13 @@
+package com.parsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
