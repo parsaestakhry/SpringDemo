76a53c8a03952b18285d380ec73d484b8e3c21d0
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-23 22:53:18 +0330

    add constructor to Alien class to log object creation

--- src/main/java/com/parsa/Alien.java
+++ src/main/java/com/parsa/Alien.java
@@ -2,6 +2,10 @@ package com.parsa;
 
 public class Alien {
 
+    public Alien() {
+        System.out.println("Object Created!");
+    }
+
     public void coding() {
         System.out.println("Coding");
     }
