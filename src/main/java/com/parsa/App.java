12d762aa0ee775c5fc404f482b99cb2726046b63
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-23 23:01:23 +0330

    update Alien bean usage to demonstrate age property assignment

--- src/main/java/com/parsa/App.java
+++ src/main/java/com/parsa/App.java
@@ -14,9 +14,15 @@ public class App
 {
     public static void main( String[] args )
     {
+//        object will be created in this line
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-//        Alien alien = (Alien)  context.getBean("alien");
-//        alien.coding();
+        Alien alien = (Alien)  context.getBean("alien");
+        Alien alien1 = context.getBean(Alien.class);
+        alien.age = 2;
+        alien1.age = 223;
+        System.out.println(alien.age);
+        System.out.println(alien1.age);
+
 
     }
 }
