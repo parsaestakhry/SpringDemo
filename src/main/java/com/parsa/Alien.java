0d0bed43cbfbfe53c062b9594e889df0f9a6e6e8
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-24 18:45:54 +0330

    make age field private and add getter/setter methods in Alien class

--- src/main/java/com/parsa/Alien.java
+++ src/main/java/com/parsa/Alien.java
@@ -3,7 +3,16 @@ package com.parsa;
 public class Alien {
 
 
-    int age;
+    private  int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("setter is called");
+        this.age = age;
+    }
 
     public Alien() {
         System.out.println("Object Created!");
